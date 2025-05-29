bd484047638cc299ac0217e78e02c017d9ad8f42
Kalll3 <84170439+Kalll3@users.noreply.github.com>
2025-05-29 15:26:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,23 +8,18 @@ st.title('My First Fvckinggg Apppp !!!')
 st.write('Welcome to my FKINGGGGGGGGGGAPPPPPPPPPPPPPPPPPPPPPPPP app!') 
 
 # Create a text input 
-widgetuser_input = st.text_input('Enter a custom message to turn into ASCII art:', 'Hello, Streamlit!') 
+widgetuser_input = st.text_input('Enter a custom message:', 'Hello, Streamlit!') 
 
-# Generate ASCII art using pyfiglet
-ascii_art = pyfiglet.figlet_format(widgetuser_input)
+# Display the customized message 
+st.write('', widgetuser_input)
 
-# Display the ASCII art inside a code block
-st.code(ascii_art, language='text')
 
-# Add a button to copy ASCII art
-st.download_button('Copy ASCII Art', ascii_art, file_name='ascii_art.txt')
-
-# API calls
+#API calls
 response = requests.get('https://api.vatcomply.com/rates?base=USD')
 
 if response.status_code == 200:
     data = response.json()
-    st.write('Exchange Rates Output:')
+    st.write('Output:')
     st.json(data)  # nicely formatted JSON output
 else:
     st.error(f"API call failed with status code: {response.status_code}")
